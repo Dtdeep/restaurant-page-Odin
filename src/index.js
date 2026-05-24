93448d5dc24883f879cc86e8f06a6690782c0ded
Dtdeep <dumagandesthiny@gmail.com>
2026-05-25 00:33:24 +0800

    make rendering of homepage work

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
+import reset from "./comeauReset.css"
 import styles from "./styles.css"
+import createHomepage from "./homepage.js";
+
 console.log("Hello Mikay");
+
+const header = document.querySelector("#header");
+
+header.addEventListener("click", (e)=>{
+    const target = e.target;
+    switch(target.id){
+        case "home":
+            createHomepage();
+            console.log("you clicked homepage")
+            break;
+        case "menu":
+            console.log("you clicked menu!")
+            break;
+        case "contact":
+            console.log("You clicked contact")
+            break;
+    }
+})
