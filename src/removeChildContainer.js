a7731bff7b5073724e7924d7927dd1acd6fed23f
Dtdeep <dumagandesthiny@gmail.com>
2026-05-25 09:11:55 +0800

    Add a function that removes all child of childContainer

--- src/removeChildContainer.js
+++ src/removeChildContainer.js
@@ -5,7 +5,5 @@ const removeAllChild = () =>{
         childContent.removeChild(childContent.firstChild);
     }
 }
-    
 
-
-export {childContent, removeAllChild}; 
+export {childContent, removeAllChild};
